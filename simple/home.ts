8a7e4f5bfb298f6a8cda7e5d98303949d83ff4ba
zerounsajid <zerounsajid@gmail.com>
2020-02-04 12:55:01 +0530

    segment tabs first

---
+++ simple/home.ts
@@ -0,0 +1,43 @@
+import { Component ,ViewChild} from '@angular/core';
+import { NavController ,Slides} from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+ @ViewChild('SwipedTabsSlider') SwipedTabsSlider: Slides ;
+
+  SwipedTabsIndicator :any= null;
+  tabs:any=[];
+
+ 
+  constructor(public navCtrl: NavController) {
+  	this.tabs=["page1","page2","page3","page4"];
+  }
+  ionViewDidEnter() {
+    this.SwipedTabsIndicator = document.getElementById("indicator");
+  }
+
+  selectTab(index) {    
+    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(100*index)+'%,0,0)';
+    this.SwipedTabsSlider.slideTo(index, 500);
+  }
+
+  updateIndicatorPosition() {
+      // this condition is to avoid passing to incorrect index
+  	if( this.SwipedTabsSlider.length()> this.SwipedTabsSlider.getActiveIndex())
+  	{
+  		this.SwipedTabsIndicator.style.webkitTransform = 'translate3d('+(this.SwipedTabsSlider.getActiveIndex() * 100)+'%,0,0)';
+  	}
+    
+    }
+
+  animateIndicator($event) {
+  	if(this.SwipedTabsIndicator)
+   	    this.SwipedTabsIndicator.style.webkitTransform = 'translate3d(' + (($event.progress* (this.SwipedTabsSlider.length()-1))*100) + '%,0,0)';
+  }
+
+ 
+
+}
